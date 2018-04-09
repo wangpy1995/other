4dc59e011c0c92f5e588a4fd433e77d885804c53
wpy <592114387@qq.com>
2018-04-09 21:57:49 +0800

    [ADD]jni DirectByteBuffer

--- bit/src/main/java/jni/JniDot.java
+++ bit/src/main/java/jni/JniDot.java
@@ -3,32 +3,14 @@ package jni;
 import java.nio.ByteBuffer;
 import java.util.Random;
 
+//-XX:MaxDirectMemorySize=6g -XX:+UnlockDiagnosticVMOptions -XX:CompileCommand=print,*JniDot.dot
 public class JniDot {
 
     static {
         System.load("/home/wpy/clion/SIMD_AVX/cmake-build-debug/libjni_Test.so");
     }
 
-    private static ByteBuffer a;
-    private static ByteBuffer b;
-    private static byte[] src = new byte[512];
-    private static byte[] des = new byte[512];
-
-    static {
-
-        Random r = new Random();
-        r.nextBytes(src);
-        r.nextBytes(des);
-
-        a = ByteBuffer.allocateDirect(512);
-        b = ByteBuffer.allocateDirect(512);
-        a.put(src);
-        b.put(des);
-    }
-
-    public native int[] dot_product_r7(int len, ByteBuffer a, ByteBuffer b0, ByteBuffer b1, ByteBuffer b2, ByteBuffer b3, ByteBuffer b4, ByteBuffer b5);
-
-    public native int dot_product(ByteBuffer a, ByteBuffer b, int len);
+    public native int dotProduct(ByteBuffer a, ByteBuffer b, int len);
 
     public int dot(byte[] a, byte[] b, int len) {
         int sum = 0;
@@ -38,29 +20,40 @@ public class JniDot {
         return sum;
     }
 
-    public static void testDot() {
+    private static void testDot() {
+        ByteBuffer a = ByteBuffer.allocateDirect(512);
+        ByteBuffer b = ByteBuffer.allocateDirect(512);
+        byte[] src = new byte[512];
+        byte[] des = new byte[512];
+        Random rnd = new Random();
+
+        rnd.nextBytes(src);
+        a.put(src);
+
         JniDot test = new JniDot();
         int s = 0;
         int r = 0;
-        int[] res = null;
 
+        b.put(des);
         long t0 = System.currentTimeMillis();
-        for (int i = 0; i < 10000000; i++)
+        for (int i = 0; i < 30000000; i++) {
+            des[0] = (byte) i;
             s = test.dot(src, des, 512);
+        }
+
         long t1 = System.currentTimeMillis();
-        for (int i = 0; i < 10000000; i++)
-            r = test.dot_product(a, b, 512);
+        for (int i = 0; i < 30000000; i++) {
+            b.put(0, (byte) i);
+            r = test.dotProduct(a, b, 512);
+        }
         long t2 = System.currentTimeMillis();
-        for (int i = 0; i < 10000000 / 6; i++)
-            res = test.dot_product_r7(512, a, b, b, b, b, b, b);
-        long t3 = System.currentTimeMillis();
 
         System.out.println("dot: " + s + "\t" + "cost: " + (t1 - t0));
         System.out.println("dot1: " + r + "\t" + "cost: " + (t2 - t1));
-        System.out.println("dot_r: " + res[5] + "\t" + "cost: " + (t3 - t2));
     }
 
     public static void main(String[] args) {
-        testDot();
+        for (int i = 0; i < 10; i++)
+            testDot();
     }
 }
--- bit/src/test/java/test/jni/JniDotTest.java
+++ bit/src/test/java/test/jni/JniDotTest.java
@@ -29,22 +29,17 @@ public class JniDotTest {
         JniDot test = new JniDot();
         int s = 0;
         int r = 0;
-        int[] res = null;
 
         long t0 = System.currentTimeMillis();
         for (int i = 0; i < 10000000; i++)
             s = test.dot(src, des, 512);
         long t1 = System.currentTimeMillis();
         for (int i = 0; i < 10000000; i++)
-            r = test.dot_product(a, b, 512);
+            r = test.dotProduct(a, b, 512);
         long t2 = System.currentTimeMillis();
-        for (int i = 0; i < 10000000 / 6; i++)
-            res = test.dot_product_r7(512, a, b, b, b, b, b, b);
-        long t3 = System.currentTimeMillis();
 
         System.out.println("dot: " + s + "\t" + "cost: " + (t1 - t0));
         System.out.println("dot1: " + r + "\t" + "cost: " + (t2 - t1));
-        System.out.println("dot_r: " + res[5] + "\t" + "cost: " + (t3 - t2));
     }
 
 }
