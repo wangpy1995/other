23ae316b3c5119882840a946e7436691391511f6
wpy <592114387@qq.com>
2018-06-15 00:54:14 +0800

    [ADD]jni batch DirectByteBuffer

--- bit/src/main/java/jni/JniDot.java
+++ bit/src/main/java/jni/JniDot.java
@@ -5,4 +5,6 @@ import java.nio.ByteBuffer;
 //-XX:MaxDirectMemorySize=6g -XX:+UnlockDiagnosticVMOptions -XX:CompileCommand=print,*JniDot.dot
 public class JniDot {
     public native int dotProduct(ByteBuffer a, ByteBuffer b);
+
+    public native void batchDotProduct(ByteBuffer a, ByteBuffer b, ByteBuffer res, int batchSize);
 }
--- bit/src/test/java/test/jni/JniDotTest.java
+++ bit/src/test/java/test/jni/JniDotTest.java
@@ -1,6 +1,7 @@
 package test.jni;
 
 import jni.JniDot;
+import jni.TestThread;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
@@ -8,6 +9,17 @@ import java.util.Random;
 
 public class JniDotTest {
     private static int NUM_ITERATIONS = 2560000;
+    private static int BATCH_SIZE = 200000;
+    private static int TIMES = NUM_ITERATIONS / BATCH_SIZE;
+
+    ByteBuffer batch = ByteBuffer.allocateDirect(512 * BATCH_SIZE);
+    ByteBuffer res = ByteBuffer.allocateDirect(4 * BATCH_SIZE);
+    ByteBuffer a = ByteBuffer.allocateDirect(512);
+    ByteBuffer b = ByteBuffer.allocateDirect(512);
+    byte[] batchSrc = new byte[512 * (BATCH_SIZE - 1)];
+    byte[] src = new byte[512];
+    byte[] des = new byte[512];
+
 
     static {
         System.load("/home/wpy/clion/SIMD_AVX/cmake-build-release/libjni_Test.so");
@@ -24,44 +36,71 @@ public class JniDotTest {
     }
 
     private void test() {
-        ByteBuffer a = ByteBuffer.allocateDirect(512);
-        ByteBuffer b = ByteBuffer.allocateDirect(512);
-        byte[] src = new byte[512];
-        byte[] des = new byte[512];
-        Random rnd = new Random();
-
-        rnd.nextBytes(src);
-        a.put(src);
-
+        int br = 0;
         int s = 0;
         int r = 0;
 
-        rnd.nextBytes(des);
-        b.put(des);
+
         long t0 = System.currentTimeMillis();
         for (int i = 0; i < NUM_ITERATIONS; i++) {
-            des[0] = (byte) i;
+//            des[0] = (byte) i;
             s = dot(src, des);
         }
 
         long t1 = System.currentTimeMillis();
         for (int i = 0; i < NUM_ITERATIONS; i++) {
-            b.put(0, (byte) i);
+//            b.put(0, (byte) i);
             r = test.dotProduct(a, b);
         }
         long t2 = System.currentTimeMillis();
 
-        a.clear();
-        b.clear();
+        for (int i = 0; i < TIMES; i++) {
+//            b.put(0, (byte) i);
+            test.batchDotProduct(batch, b, res, BATCH_SIZE);
 
-        System.out.println("dot: " + s + "\t" + "cost: " + (t1 - t0));
-        System.out.println("dot1: " + r + "\t" + "cost: " + (t2 - t1));
+            //小端
+            byte r0 = res.get(0);
+            byte r1 = res.get(1);
+            byte r2 = res.get(2);
+            byte r3 = res.get(3);
+            br = (r3 << 24) + (r2 << 16) + (r1 << 8) + r0;
+        }
+        long t3 = System.currentTimeMillis();
+
+        System.out.println("dot: " + s + "\t\t" + "cost: " + (t1 - t0));
+        System.out.println("dot1: " + r + "\t\t" + "cost: " + (t2 - t1));
+        System.out.println("batch dot: " + br + "\t" + "cost: " + (t3 - t2));
     }
 
     @Test
     public void testDot() {
-//        for (int i = 0; i < 100; i++)
-            test();
+
+        Random rnd = new Random();
+
+        rnd.nextBytes(batchSrc);
+
+        rnd.nextBytes(src);
+        a.put(src);
+
+        batch.put(src);
+//        batch.put(batchSrc);
+
+        rnd.nextBytes(des);
+        b.put(des);
+
+        for (int i = 0; i < 10; i++) test();
+        batch.clear();
+        a.clear();
+        b.clear();
+
+    }
+
+    @Test
+    public void testAdd() {
+        TestThread t1 = new TestThread();
+        System.out.println(t1.add(1));
+        TestThread t2 = new TestThread();
+        System.out.println(t2.add(1));
     }
 
 }
